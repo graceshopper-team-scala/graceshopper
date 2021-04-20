99d80b85a9e3d126a729332e1655d7dfe7d36162
Tajinder Singh <Tj204268122@gmail.com>
2021-04-20 17:50:05 -0400

    added navbar functionality

--- client/app.js
+++ client/app.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import Navbar from './components/Navbar'
+import NavBar from './components/Navbar'
 import Routes from './routes'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <NavBar />
       <Routes />
     </div>
   )
