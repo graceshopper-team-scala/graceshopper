3f08b8e57a8f8e7f1b12bd3ae37ccf2e514d42e0
Jake Chen <jakechen825@gmail.com>
2021-04-20 18:23:29 -0400

    deleted single vehicle css

--- client/components/SingleVehicleScreen.js
+++ client/components/SingleVehicleScreen.js
@@ -1,4 +1,3 @@
-import './SingleVehicleScreen.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getSingleVehicleThunk } from '../store/singleVehicle';
