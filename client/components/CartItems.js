4dcdf792b72eff14449433dd8eefb77e0a4ec461
Jake Chen <jakechen825@gmail.com>
2021-04-21 15:51:20 -0400

    (FE) Added change input to select and added in trash icon from FAS

--- client/components/CartItems.js
+++ client/components/CartItems.js
@@ -1,32 +1,42 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class CartItems extends Component {
   render() {
     const { items } = this.props;
+
     return (
       <table className="cart-items">
-        <tr>
-          <th>Item</th>
-          <th>Quantity</th>
-          <th>Price</th>
-        </tr>
-        {items.map((item) => {
-          return (
-            <tr className="single-cart-item">
-              <td>
-                <img src={item.imageUrl} />
-                <Link to={`/vehicles/${item.id}`}>
-                  {item.make} {item.model}{" "}
-                </Link>
-              </td>
-              <td>
-                <input value={1} />
-              </td>
-              <td>{item.price}</td>
-            </tr>
-          );
-        })}
+        <tbody>
+          <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+          {items.map((item) => {
+            return (
+              <tr className="single-cart-item" key={item.id}>
+                <td>
+                  <img src={item.imageUrl} />
+                  <Link to={`/vehicles/${item.id}`} className="cartitem_name">
+                    {item.make} {item.model}{' '}
+                  </Link>
+                </td>
+                <td>
+                  <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </select>
+                  <button>
+                    <i className="fas fa-trash"></i>
+                  </button>
+                </td>
+                <td>{item.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     );
   }
--- client/store/index.js
+++ client/store/index.js
@@ -1,11 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
-import thunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import auth from "./auth";
-import allVehicles from "./allVehicles";
-import singleVehicle from "./singleVehicle";
-import cartReducer from "./cart";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import auth from './auth';
+import allVehicles from './allVehicles';
+import singleVehicle from './singleVehicle';
+import cartReducer from './cart';
+
 const reducer = combineReducers({
   auth,
   vehicles: allVehicles,
@@ -18,4 +19,4 @@ const middleware = composeWithDevTools(
 const store = createStore(reducer, middleware);
 
 export default store;
-export * from "./auth";
+export * from './auth';
